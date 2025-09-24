46e56189645ff901f6ee0e0e7aa3ea8a960db2ad
Emre <emrebingor5@gmail.com>
2025-09-24 15:31:36 +0300

    Initial Commit

---
+++ Bibble/BibbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibbleApp.swift
+//  Bibble
+//
+//  Created by Emre Bingör on 24.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct BibbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
